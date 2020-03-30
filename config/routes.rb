1d2d5f776cb6ee2838cb4722892d31444a89dca7
ACAPP-dev <acapp909@gmail.com>
2020-03-30 10:26:45 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/show', to: 'secrets#show'
+
+  get '/new', to: 'sessions#new'
+
+  post '/create', to: 'sessions#create'
+
+  delete '/destroy', to: 'sessions#destroy'
 end
